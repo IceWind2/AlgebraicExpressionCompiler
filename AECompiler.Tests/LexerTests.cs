b3b9d7f28e447774dd5d385dc4d001eb16f47cf9
Anton Glazyrin <a.glazyrin@spbu.ru>
2022-08-09 20:31:41 +0300

    Testing: add unit tests for Lexer and Parser

---
+++ AECompiler.Tests/LexerTests.cs
@@ -0,0 +1,106 @@
+using AECompiler.Core.AST.Tokens;
+using AECompiler.Core.Lexers;
+
+namespace AECompiler.Tests;
+
+public class LexerTests
+{
+    [Fact]
+    public void Tokenize_NullExpression_ContainsEmptyString()
+    {
+        var lexer = new Lexer();
+
+        lexer.Tokenize(null);
+
+        Assert.Empty(lexer.Expression);
+    }
+
+    [Fact]
+    public void Tokenize_StringExpression_ContainsSameString()
+    {
+        var lexer = new Lexer();
+        const string expression = "0";
+
+        lexer.Tokenize(expression);
+
+        Assert.Equal(expression, lexer.Expression);
+    }
+
+    [Fact]  
+    public void GetNextToken_EmptyExpression_ReturnsEOFToken()
+    {
+        var lexer = new Lexer();
+        
+        Token token = lexer.GetNextToken();
+
+        Assert.Equal(TokenType.Eof, token.Type);
+    }
+
+    [Theory]
+    [InlineData("0")]
+    [InlineData("241  ")]
+    [InlineData("  3443")]
+    public void GetNextToken_Number_ReturnsSameNumber(string expression)
+    {
+        var lexer = new Lexer(expression);
+
+        Token token = lexer.GetNextToken();
+
+        Assert.Equal(expression.Trim(), token.Value.ToString());
+    }
+
+    [Theory]
+    [InlineData("+")]
+    [InlineData("   *  ")]
+    public void GetNextToken_OperationSymbol_ReturnsSameSymbol(string expression)
+    {
+        var lexer = new Lexer(expression);
+
+        Token token = lexer.GetNextToken();
+
+        Assert.Equal(expression.Trim(), token.Value.ToString());
+    }
+
+    [Fact]
+    public void GetNextToken_InvalidSymbol_ThrowsArgumentException()
+    {
+        const string expression = "$#@";
+        var lexer = new Lexer(expression);
+
+        Assert.Throws<ArgumentException>(()=>lexer.GetNextToken());
+    }
+
+    [Fact]
+    public void GetNextToken_SeveralTokens_ReturnsSecondToken()
+    {
+        const string expression = "232 + 55";
+        var lexer = new Lexer(expression);
+
+        lexer.GetNextToken();
+        Token token = lexer.GetNextToken();
+
+        Assert.Equal("+", token.Value.ToString());
+    }
+
+    [Fact]
+    public void GetLastProcessedToken_EmptyExpression_ReturnsNull()
+    {
+        var lexer = new Lexer();
+
+        Token token = lexer.GetLastProcessedToken();
+
+        Assert.Equal(TokenType.Empty, token.Type);
+    }
+
+    [Fact]
+    public void GetLastProcessedToken_Token_ReturnsSameToken()
+    {
+        const string expression = "232";
+        var lexer = new Lexer(expression);
+
+        lexer.GetNextToken();
+        Token token = lexer.GetLastProcessedToken();
+
+        Assert.Equal(expression, token.Value.ToString());
+    }
+}
---
+++ AECompiler.Tests/ParserTests.cs
@@ -0,0 +1,58 @@
+﻿using AECompiler.Core.AST.Nodes;
+using AECompiler.Core.AST.Tokens;
+using AECompiler.Core.Lexers;
+using AECompiler.Core.Parsers;
+
+namespace AECompiler.Tests;
+
+public class ParserTests
+{
+    [Fact]
+    public void Parse_EmptyExpression_ReturnsEmptyToken()
+    {
+        var parser = CreateDefaultParser();
+        const string expression = "";
+
+        ASTNode root = parser.Parse(expression);
+
+        Assert.Equal(TokenType.Empty, root.GetToken().Type);
+    }
+
+    [Fact]
+    public void Parse_Number_ReturnsIntToken()
+    {
+        var parser = CreateDefaultParser();
+        const string expression = "0";
+
+        ASTNode root = parser.Parse(expression);
+
+        Assert.Equal(TokenType.Int, root.GetToken().Type);
+    }
+    
+    [Fact]
+    public void Parse_OpSymbol_ThrowsException()
+    {
+        var parser = CreateDefaultParser();
+        const string expression = "+";
+
+       Assert.Throws<ArgumentException>(() => parser.Parse(expression));
+    }
+    
+    [Fact]
+    public void Parse_Expression_ReturnsOpToken()
+    {
+        var parser = CreateDefaultParser();
+        const string expression = "0 + 0";
+
+        ASTNode root = parser.Parse(expression);
+
+        Assert.Equal(TokenType.Plus, root.GetToken().Type);
+    }
+    
+    private Parser CreateDefaultParser()
+    {
+        var lexer = new Lexer();
+
+        return new Parser(lexer);
+    } 
+}
